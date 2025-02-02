c5b6fcf872d65d2f1824816af5b4cc5c3712eb53
Sherdorbek <sherdor20040104@gmail.com>
2025-02-02 16:13:45 +0500

    Update dotfiles: Sun Feb  2 04:13:45 PM +05 2025

---
+++ wofi/power-menu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the menu options
+entries="⇠ Logout\n⏾ Sleep\n⭮ Reboot\n⏻ Shutdown"
+
+# Show menu and get user selection
+selected=$(echo -e $entries | wofi --dmenu --cache-file=/dev/null --prompt="Power Menu" --width=400 --height=200 --style=$HOME/.config/wofi/style.css)
+
+# Execute the selected option
+case $selected in
+    "⇠ Logout")
+        swaymsg exit
+        ;;
+    "⏾ Sleep")
+        systemctl suspend
+        ;;
+    "⭮ Reboot")
+        systemctl reboot
+        ;;
+    "⏻ Shutdown")
+        systemctl poweroff
+        ;;
+esac
